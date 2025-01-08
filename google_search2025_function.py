66f4ffd13baa90c5f8acd04d591ff3db4dd79d82
Anzor <anzor.epitashvili@gmail.com>
2025-01-08 15:02:58 -0500

    refactored google search. parametarize (make it function)

---
+++ google_search2025_function.py
@@ -0,0 +1,52 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+
+def google_search(search_phrase):
+    print("# Scenario: verify the google search results")
+    print("# open the browser")
+    driver = webdriver.Chrome() # initializes the browser, lunches the driver
+    print('name of the browser:', driver.name)
+    driver.maximize_window()
+    #time.sleep(5)
+
+    print("# open the website google.com")
+    driver.get("https://www.google.com")
+    time.sleep(1)
+
+    print("# enter 'selenium' ")
+    # Locators: ways to finding element on HTML
+    # ID, Name, xpath, Class name, css selector, link text, partial link text
+    # search_box = driver.find_element(By.ID, 'APjFqb') #id="APjFqb", MAKE Sure it's uniqe on HTML
+    search_box = driver.find_element(By.NAME, 'q') # name="q"
+    search_box.send_keys(search_phrase)
+    print("# then hit Enter button")
+    search_box.send_keys(Keys.ENTER)
+
+    print("# get the text of Search result ' about 277,000,000 results (0.66 seconds)")
+    result_msg = driver.find_element(By.ID,'result-stats').text #id="result-stats"
+    print("extracted massage:", result_msg)
+    print("# then get the number of results from the message")
+    # use split() function to break the message into smaller text based on space delimiter. returs a list
+    number_text = result_msg.split()[1]
+    print("number_text:", number_text)
+    # use replace() function, replace(',', '')
+    number_text_removed_comma = number_text.replace(',', '')
+    print("result_num, removed comma:", number_text_removed_comma)
+    print("is it a number?", type(number_text_removed_comma))
+    result_num = int(number_text_removed_comma)
+    print("is it a number:", type(result_num))
+
+    print("# verify it is more than 200000")
+    print("is search returned more than 200 mln results:", result_num > 200000000)
+    assert result_num > 200000000, "FAIL. result number verification failed"
+
+    time.sleep(15)
+    print("Google search scenario completed!")
+
+google_search('selenium')
+google_search('python')
+google_search('test automation')
