2b0b9ec9b761d3a1e86d3ff5c7ed661149396305
Anzor <anzor.epitashvili@gmail.com>
2025-01-11 13:25:11 -0500

    update webDriver script

--- src1/web_driver/web_driver.py
+++ src1/web_driver/web_driver.py
@@ -9,13 +9,15 @@ from src1.utilites import *
 
 URL = "https://demoqa.com/browser-windows"
 # locators
-
-print("# webDriver class attributes/properties")
+print("Initializing the browser, with chrome options")
 chr_options = Options()
 chr_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chr_options)  # instantiating webDriver class, driver object created
 driver.get(URL)
+print("disabling google adds")
 time.sleep(0.5)
 disable_google_ads(driver)
-print('name of the browser:', driver.name)
+
+print("# webDriver class attributes/properties")
+print('name of the browser:', driver.name) # property
 driver.maximize_window()
