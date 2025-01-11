cbcf6c29cc43082d66f904456253ddc4368a518d
Anzor <anzor.epitashvili@gmail.com>
2025-01-11 15:53:30 -0500

    update webDriver script

--- src1/web_driver/web_driver.py
+++ src1/web_driver/web_driver.py
@@ -14,10 +14,41 @@ chr_options = Options()
 chr_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chr_options)  # instantiating webDriver class, driver object created
 driver.get(URL)
+driver.maximize_window()
+
 print("disabling google adds")
 time.sleep(0.5)
 disable_google_ads(driver)
 
 print("# webDriver class attributes/properties")
 print('name of the browser:', driver.name) # property
-driver.maximize_window()
+print('title of the webpage', driver.title)
+print('current url of page', driver.current_url)
+print("ID of the current browser tab or window:", driver.current_window_handle)
+
+print(" # webDriver class method:  ")
+driver.back()
+print("current url", driver.current_url) # ' data:, '
+assert driver.current_url == 'data:,'
+
+driver.forward()
+time.sleep(0.5)
+disable_google_ads(driver)
+assert driver.current_url == 'https://demoqa.com/browser-windows', 'url verification failed on forward()'
+time.sleep(2)
+
+driver.refresh()
+time.sleep(0.5)
+disable_google_ads(driver)
+assert driver.current_url == 'https://demoqa.com/browser-windows', 'url verification failed on refresh()'
+
+print("switching between tabs...")
+print("find the element, click on it, it should open the new tab")
+print("switch to the new tab")
+print("verify we are on the new tab: find the element with the text, verify the text as expected")
+
+
+
+print("closing browser...")
+time.sleep(5)
+driver.quit()
