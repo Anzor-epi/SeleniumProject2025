5e389781727e570c92cde1ca212bfcd6be73ea80
Anzor <anzor.epitashvili@gmail.com>
2025-01-13 18:18:04 -0500

    added scenario 2 : enter flight details

--- src1/tests/test_webelement_forms.py
+++ src1/tests/test_webelement_forms.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.support.select import Select
+
 from src1.web_driver.webelements_forms import *
 
 #URL = "https://demoqa.com/browser-windows"
@@ -9,17 +11,32 @@ passw= 'testwise'
 # Execution:
 driver = initialize_browser()
 
-print("Scenario 1: agileway travel website")
+print("****** Scenario 1: agileway travel website ****** ")
 open_website(driver, URL)
 signin(driver, user, passw)
 singoff(driver)
-close_browser(driver)
+print("****** Scenario 1: completed ****** ")
 
-print("Scenario 2: select a flights")
+print("******  Scenario 2: select a flights ****** ")
 open_website(driver, URL)
 signin(driver, user, passw)
-# flights: select a flights
-# flights: enter passenger details
-# flights: pay the ticket, get confirmation
+enter_flights_details(driver,'New York', 'Sydney')
+#enter_flights_details(driver,'Sydney', 'New York')
+enter_passenger_details(driver)
+pay_ticket(driver)
+
+
+
+# H/W flights : enter passenger details
+# verify the text:   2024-01-01 New York to Sydney,  2024-02-01 Sydney to New York
+# click next verify the flash_alert, get the text to verify
+# enter lastname , firstname
+# click on Next
+
+# h/w flights : pay the ticket, get confirmation
+# return booking number
+# booking_number = driver.find_element(By.XPATH, "//span[@id='booking_number']").text
+
 singoff(driver)
+print("****** Scenario 2: completed ****** ")
 close_browser(driver)
--- src1/web_driver/webelements_forms.py
+++ src1/web_driver/webelements_forms.py
@@ -3,6 +3,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
 
 from src1.utilites import *
 
@@ -88,6 +89,58 @@ def close_browser(driver):
     driver.quit()
 
 
+def enter_flights_details(driver, from_city, to_city):
+    # locators
+    trip_type_xpath= "//input[@value='return']"
+    origin_name = 'fromPort'
+    destination_name = 'toPort'
+    second_checkbox_xpath = '//table[@id="flights"]/tbody/tr[2]/th/input'
+    continue_xpath = "//input[@value='Continue']"
+
+    print("# flights: select a flights")
+    trip_type = driver.find_element(By.XPATH, trip_type_xpath)
+    print(f"is return trip type selected:, {trip_type.is_selected()}")
+
+    # check="true" - means returns should be selected by default
+    print(f"attribute value: {trip_type.get_attribute('checked')}")
+    print("select origin: New York")
+    origin = Select(driver.find_element(By.NAME, origin_name))
+    origin.select_by_visible_text(from_city)
+    origin.select_by_index(1)
+
+    print("select destination: Sydney")
+    destination = Select(driver.find_element(By.NAME, destination_name))
+    destination.select_by_visible_text(to_city)
+
+    print("Select departure month and year")
+    departing = Select(driver.find_element(By.ID, 'departMonth'))
+    # departing.select_by_visible_text('January 2025')
+    departing.select_by_value('012025')
+
+    print("Select return month and year")
+    returning = Select(driver.find_element(By.ID, 'returnMonth'))
+    # returning.select_by_visible_text('March 2025')
+    # returning.select_by_value('032025')
+    returning.select_by_index(2)
+
+    print("select second flight from the list")
+    second_checkbox = driver.find_element(By.XPATH, second_checkbox_xpath)
+    second_checkbox.click()
+    print(f"is flight checked: {second_checkbox.is_selected()}")
+
+    print("continue to next page")
+    # driver.find_element(By.XPATH, "//input[@value='Continue' and @type='submit']").click()
+    driver.find_element(By.XPATH, continue_xpath).click()
+
+
+def enter_passenger_details(driver):
+    """ create steps in test files then transfer here """
+    pass
+
+
+def pay_ticket(driver):
+    """ create steps in test files then transfer here """
+    pass
 
 # Execution open test_webelements_forms
 
