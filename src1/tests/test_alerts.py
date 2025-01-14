ae1a3f146f0a0510eae4a82d5d93c0edcf1f83a5
Anzor <anzor.epitashvili@gmail.com>
2025-01-14 13:23:06 -0500

    Alters: Scenario 1 Test case 1 completed. (get Alert text, click Alert Ok button)

--- src1/tests/test_alerts.py
+++ src1/tests/test_alerts.py
@@ -9,7 +9,26 @@ driver = initialize_browser()
 open_website(driver, URL)
 
 print("********** Scenario 1: automating alert interaction started ********")
+print("********** Test case 1: getting text and clicking Ok button on Alert********")
+
+print('# click on "click me" button for Ok-Cencel case')
+confirm_button_xpath = '//button[@id="confirmButton"]'
+driver.find_element(By.XPATH, confirm_button_xpath).click()
+
+alert_box = driver.switch_to.alert
+print(f"massage on the alert box: {alert_box.text} ------")
+print('# clicking Ok on Alert')
+alert_box.accept() # clicking ok button alert
 time.sleep(2)
+
+# than verifying green text showing " You selected Ok"
+confirm_result_xpath = '//span[@id="confirmResult"]'
+confirm_result_msg = driver.find_element(By.XPATH, confirm_result_xpath).text
+print(f"confirmation result: '{confirm_result_msg}' ---- ")
+assert confirm_result_msg.strip() == 'You selected Ok', 'ERROR: confirmation text verification FAILED!!'
+time.sleep(10)
+print("********** Test case 1: Completed  ********")
+
 
 print("********* Scenario 1: automating alert interaction completed ********")
 
