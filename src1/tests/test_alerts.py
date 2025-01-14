5083b6457cc0b0ac3a1351f114a5b1ffbad9cd69
Anzor <anzor.epitashvili@gmail.com>
2025-01-14 14:51:03 -0500

    Alters: Scenario 1 Test case 2 completed. (get Alert text, click Alert Cancel button)

--- src1/tests/test_alerts.py
+++ src1/tests/test_alerts.py
@@ -9,26 +9,48 @@ driver = initialize_browser()
 open_website(driver, URL)
 
 print("********** Scenario 1: automating alert interaction started ********")
+
 print("********** Test case 1: getting text and clicking Ok button on Alert********")
 
 print('# click on "click me" button for Ok-Cencel case')
 confirm_button_xpath = '//button[@id="confirmButton"]'
-driver.find_element(By.XPATH, confirm_button_xpath).click()
+click_me3 = driver.find_element(By.XPATH, confirm_button_xpath)
+click_me3.click()
 
 alert_box = driver.switch_to.alert
 print(f"massage on the alert box: {alert_box.text} ------")
 print('# clicking Ok on Alert')
 alert_box.accept() # clicking ok button alert
-time.sleep(2)
+time.sleep(0.5)
 
 # than verifying green text showing " You selected Ok"
 confirm_result_xpath = '//span[@id="confirmResult"]'
 confirm_result_msg = driver.find_element(By.XPATH, confirm_result_xpath).text
 print(f"confirmation result: '{confirm_result_msg}' ---- ")
 assert confirm_result_msg.strip() == 'You selected Ok', 'ERROR: confirmation text verification FAILED!!'
-time.sleep(10)
+time.sleep(0.5)
 print("********** Test case 1: Completed  ********")
 
+########################################################
+
+print("********** Test case 2: getting text and clicking Ok button on Alert********")
+print('# click on "click me" button for Ok-Cencel case')
+confirm_button_xpath = '//button[@id="confirmButton"]'
+click_me3 = driver.find_element(By.XPATH, confirm_button_xpath)
+click_me3.click()
+
+print("# Click on Cancel")
+alert_box = driver.switch_to.alert
+alert_box.dismiss() # clicking Cancel on the Alert window
+time.sleep(0.5)
+
+print("# then verifying green text showing 'You selected Cancel' ")
+confirm_result_msg = driver.find_element(By.XPATH, confirm_result_xpath).text
+print(f"confirmation result: '{confirm_result_msg}' ---- ")
+assert confirm_result_msg.strip() == 'You selected Cancel', 'ERROR: confirmation text verification FAILED!!'
+time.sleep(0.5)
+
+print("********** Test case 2: Completed  ********")
 
 print("********* Scenario 1: automating alert interaction completed ********")
 
